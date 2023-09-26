6ecdaeff1e1d515b11752648208ff3ddc0d4e2cc
AHMAD KALASH <114625236+AHMADFATAHIKALASH@users.noreply.github.com>
2023-09-25 23:10:19 -0300

    criei DAO

---
+++ DAO.py
@@ -0,0 +1,19 @@
+from Models import *
+
+
+class DaoCategoria:
+    @classmethod
+    def salvar(cls, categoria):
+        with open('categoria.txt', 'a') as arq:
+            arq.writelines(categoria)
+            arq.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('categoria.txt', 'r') as arq:
+            cls.categoria = arq.readlines()
+        cls.categoria = list(map(lambda x: x.replace('\n', ''), cls.categoria))
+        print(cls.categoria)
+
+
+DaoCategoria.ler()
